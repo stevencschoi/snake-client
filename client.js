c9f18f21721a4a4697ee456ccc3a98a69934bbde
Steven Choi <stevencschoi87@gmail.com>
2020-02-12 20:28:48 +0000

    added constants

--- client.js
+++ client.js
@@ -1,9 +1,12 @@
 const net = require('net');
+const { IP, PORT } = require('./constants');
 
 const connect = function() {
   const conn = net.createConnection({
-    host: '172.46.0.236',
-    port: 50541
+    // host: '172.46.0.236',
+    // port: 50541
+    host: IP,
+    port: PORT
   });
   // interpret incoming data as text
   conn.setEncoding('utf8');
