9f134c78a2bacacd935b58519642aa170b545955
Steven Choi <stevencschoi87@gmail.com>
2020-02-12 19:19:27 +0000

    ran linter and moved input functionality into input file

--- play.js
+++ play.js
@@ -1,23 +1,7 @@
 const { connect } = require('./client');
-
-
-const setupInput = function() {
-  const handleUserInput = function(key) {
-    if (key === '\u0003') {
-      process.exit();
-    }
-  };
-
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding('utf8');
-  stdin.resume();
-  stdin.on('data', handleUserInput);
-  return stdin;
-};
-
-
-setupInput();
+const { setupInput } = require('./input');
 
 console.log('Connecting ...');
 connect();
+
+setupInput();
