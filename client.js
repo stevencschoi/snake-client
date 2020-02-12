479267bcdc2d01bc08cd3c9df6d0ba2a6c62a91a
Steven Choi <stevencschoi87@gmail.com>
2020-02-12 18:03:25 +0000

    added functionality to connect to snake game

---
+++ client.js
@@ -0,0 +1,22 @@
+const net = require('net');
+
+const connect = function() {
+  const conn = net.createConnection({ 
+    host: '172.46.0.236',
+    port: 50541
+  });
+  // interpret incoming data as text
+  conn.setEncoding('utf8');
+
+  conn.on('data', (data) => {
+    console.log('Server says: ', data);
+  });
+
+  conn.on('connect', () => {
+    console.log('Successfully connected to the game server');
+  });
+
+  return conn;
+}
+
+module.exports = {connect}
---
+++ play.js
@@ -0,0 +1,7 @@
+const { connect } = require('./client');
+
+//  * Establishes connection with the game server
+//  */
+
+console.log('Connecting ...');
+connect();
